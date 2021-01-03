d48b88d197c1c57ff91a356792d2f8df8a4096ac
Quanterro <quanterroz@gmail.com>
2021-01-03 20:24:44 +0100

    Tests for application

--- cnnect/src/test/java/com/abr/cnnect/api/PersonControllerTest.java
+++ cnnect/src/test/java/com/abr/cnnect/api/PersonControllerTest.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;import static org.junit.Assert.assertTrue;import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
